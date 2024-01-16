667989bd3cf6c09b29f3b19050f94fd5006f5974
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-01-16 15:46:22 -0800

    Add Pages

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+const GetCPUResponse = async () => {
+    const response = await fetch('https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption');
+    const text = await response.text();
+    console.log(text)
+}
+
+function CompareResponse()
+{
+    
+}
