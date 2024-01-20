7c2101ddccb6bf1675fb2ebfb3bb4fa98eaba2bd
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-01-20 00:34:26 -0800

    Further Update #3

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,24 +4,47 @@ const scissors = 2;
 const lizard = 3;
 const spock = 4;
 
+let roundCurrent = 1;
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+let timerText = document.getElementById('timerElementID');
+
+let buttonRock = document.getElementById('buttonRock');
+let buttonPaper = document.getElementById('buttonPaper');
+let buttonScissors = document.getElementById('buttonScisssors');
+let buttonLizard = document.getElementById('buttonLizard');
+let buttonSpock = document.getElementById('buttonSpock');
+
 const GetCPUResponse = async () => {
     const response = await fetch('https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption');
     const text = await response.text();
     console.log(text);
 }
 
-function CompareResponse(player1, player2)
+let timer = 60;
+function myFunction() {
+    timer--;
+    timerText.innerText = timer;
+}
+
+setInterval(myFunction, 1000);
+
+buttonRock.addEventListener('click', function() {
+    alert('Test');
+});
+
+
+
+
+
+
+function CompareAnswers(player1, player2)
 {
     switch (player1) 
     {
         case rock:
-            if (player2 == scissors) {
-                
-            } else if (player == lizard) {
 
-            } else {
-
-            }
         break;
 
         case paper:
